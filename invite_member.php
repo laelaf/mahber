da401d3a69cefe3b8375915edd0175f5ed896b9e
eb2048 <93728733+eb2048@users.noreply.github.com>
2023-11-29 21:19:06 -0500

    page titles renamed

--- invite_member.php
+++ invite_member.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang='en'>
 <head>
     <meta charset="utf-8">
-    <title>BOOTSTRAP Mahber Home</title>
+    <title>Invite Member</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- bootstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
